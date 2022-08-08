1d101e85d6c28dd271db6a18343d9d6125c96522
Selvi <slvkesuma@gmail.com>
2022-08-08 17:58:05 +0300

    finished 2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,15 +4,25 @@ import axios from 'axios';
 const FindCountries = ({ handleFindCountriesOnChange }) => {
 	return (
 		<div>
-			find countries <input onChange={handleFindCountriesOnChange} />
+			Find countries: <input onChange={handleFindCountriesOnChange} />
 		</div>
 	)
 }
 
-const ListCountries = ({ filteredCountry }) => {
+const ListCountries = ({ filteredCountry, allCountries }) => {
 	if (filteredCountry.length > 10) {
 		return (
-			<p>Too many matches, specify another filter</p>
+			<p>Too many matches, specify another filter.</p>
+		)
+	}
+	else if (filteredCountry.length < 1) {
+		return (
+			<p>No match found.</p>
+		)
+	}
+	else if (filteredCountry.length === 1) {
+		return (
+			<DetailedResult allCountries={allCountries} filteredCountry={filteredCountry[0]} />
 		)
 	}
 	return (
@@ -20,41 +30,64 @@ const ListCountries = ({ filteredCountry }) => {
 	)
 }
 
-function App() {
-	const [allCountries, setAllCountries] = useState('')
-	const [countriesName, setCountries] = useState([])
+const DetailedResult = ({ allCountries, filteredCountry }) => {
+	let countryDetails = allCountries.filter(x => x.name.official.includes(filteredCountry))
+	countryDetails = countryDetails[0]
+
+	let capital = countryDetails.capital
+	if (capital.length > 1) {
+		capital = countryDetails.capital.map(x => <li>{x}</li>)
+	}
+
+	let area = countryDetails.area
+
+	let languages = Object.values(countryDetails.languages).map(x => <li key={x}>{x}</li>)
+
+	console.log(countryDetails);
+	return (
+		<div>
+			<h2>{countryDetails.name.official}</h2>
+			<p>Capital:</p>
+			<ul>
+				{capital}
+			</ul>
+			<p>Area: {area}</p>
+			<p><b>Languages:</b></p>
+			<ul>
+				{languages}
+			</ul>
+			<p>Flag:</p>
+			<img alt={countryDetails.name.official} width="250px" src={countryDetails.flags.svg} />
+
+		</div>
+	)
+}
+
+const App = () => {
+	const [allCountries, setAllCountries] = useState([])
+	const [countriesName, setCountriesName] = useState([])
 	const [findCountry, setFindCountry] = useState('')
-	const [capital, setCapital] = useState('')
-	const [area, setArea] = useState('')
-	const [languages, setLanguages] = useState([])
 
 	useEffect(() => {
 		axios
 			.get('https://restcountries.com/v3.1/all')
 			.then(response => {
-				console.log('promise fulfilled');
 				setAllCountries(response.data)
-				console.log(allCountries);
-				setCountries(response.data.map(x => x.name.common))
-				// setCapital(response.data.map(x => x.capital))
-				// setArea(response.data.map(x => x.area))
-				// setLanguages(response.data.languages)
 			})
 	}, [])
 
 	const handleFindCountriesOnChange = (event) => {
+		setCountriesName(allCountries.map(x => x.name.official))
 		setFindCountry(event.target.value)
-		console.log(findCountry);
+		// console.log(findCountry);
 	}
 
-	const filteredCountry = countriesName.filter(x => x.includes(findCountry))
+	const filteredCountry = countriesName.filter(x => x.toLowerCase().includes(findCountry.toLocaleLowerCase()))
 
-
-	console.log(test);
 	return (
 		<div>
 			<FindCountries handleFindCountriesOnChange={handleFindCountriesOnChange} />
-			<ListCountries filteredCountry={filteredCountry} />
+			<ListCountries filteredCountry={filteredCountry} allCountries={allCountries} />
 		</div>
 	)
 }
