8322fc16805315cd0d5e40869d13d57e3b4c4187
Selvi <slvkesuma@gmail.com>
2022-08-10 17:48:49 +0300

    updated codealong to follow material

--- part2/codealong/src/App.js
+++ part2/codealong/src/App.js
@@ -1,22 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
 	const [notes, setNotes] = useState([])
 	const [newNote, setNewNote] = useState('a new note...')
 	const [showAll, setShowAll] = useState(true)
-
-	// const hook = () => {
-	// 	console.log('effect');
-	// 	axios
-	// 		.get('http://localhost:3001/nots')
-	// 		.then(response => {
-	// 			console.log('promise fulfilled');
-	// 			setNotes(response.data)
-	// 		})
-	// }
-	// useEffect(hook, [])
 
 	useEffect(() => {
 		console.log('effect');
@@ -36,16 +26,31 @@ const App = () => {
 			content: newNote,
 			date: new Date().toISOString(),
 			important: Math.random() < 0.5,
-			id: notes.length + 1,
+			// id: notes.length + 1,
 		}
-
-		setNotes(notes.concat(noteObject))
-		setNewNote('')
+		axios
+			.post('http://localhost:3001/notes', noteObject)
+			.then(response => {
+				setNotes(notes.concat(response.data))
+				setNewNote('')
+			})
 	}
 
 	const handleNoteChange = (event) => {
 		console.log(event.target.value);
 		setNewNote(event.target.value)
+	}
+
+	const toggleImportance = (id) => {
+		// console.log('importance of ' + id + ' needs to be toggled');
+		const url = `http://localhost:3001/${id}`
+		const note = notes.find(n => n.id === id)
+		const changedNote = { ...note, important: !note.important }
+
+		axios.put(url, changedNote)
+			.then(response => {
+				setNotes(notes.map(note => note.id !== id ? note : response.data))
+			})
 	}
 
 	const noteToShow = showAll
@@ -62,7 +67,7 @@ const App = () => {
 			</div>
 			<ul>
 				{noteToShow.map(note =>
-					<Note key={note.id} note={note} />
+					<Note key={note.id} note={note} toggleImportance={() => toggleImportance(note.id)} />
 				)}
 			</ul>
 			<form onSubmit={addNote}>
